ec88bc0e52abd4924af3801c211052209ae6b9b1
mohassin <mohassin.mp@cognizant.com>
2026-09-17 17:35:38 +0100

    feature-450-model name

---
+++ src/server/model-name/content.js
@@ -0,0 +1,15 @@
+export const modelNameContent = {
+  en: {
+    heading: (modelName) => `What is the model name?`,
+    question: 'What is the model name?',
+    label: 'Model name',
+    saveButton: 'Save changes',
+    cancel: 'Cancel',
+    guidance: 'Enter the model name for this appliance.',
+    errors: {
+      summaryTitle: 'There is a problem',
+      modelNameRequired: 'Enter the model name',
+      generic: 'Sorry, there is a problem with the service'
+    }
+  }
+}
